f036631d8df83630f10845ae305aefa8f5bb199f
Dan Hewlett <danchewlett@sleepystrix.com>
2017-07-13 16:40:42 -0500

    minor fixeds

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-console.log("Midnight js wrapper here");
+//console.log("Midnight js wrapper here");
 
 var exec = require("child_process").exec;
 let path = require('path');
 let parse = function (inputText, callback) {
-	var cli_path = '"' + __dirname + '\\midnight-cli.rb' + '"'; //absolute path to midnight-cli.rb
-	var command = 'ruby ' +  cli_path + ' "' + inputText + '"'; //command to exec
-	exec(command, function (err, stdout, stderr) {
+	var cli_path = path.join(__dirname, 'midnight-cli.rb'); //absolute path to midnight-cli.rb
+	//var command = 'bundle exec ruby -rubygems ' + cli_path + ' "' + inputText + '"'; //command to exec
+	var command = 'ruby -rubygems ' + cli_path + ' "' + inputText + '"'; //command to exec
+	exec(command, {
+		cwd: __dirname
+	}, function (err, stdout, stderr) {
 		if (err) {
-			console.log(err);
-			callback(err, null)
+			//console.log(err);
+			callback(err, null);
+		} else if (stderr) {
+			callback(stderr, null);
 		} else {
-			console.log(stdout);
-			callback(null, stdout);
+			//console.log(stdout);
+			var output = stdout;
+			output = output.split("\n").join("");
+			output = output.split("\r").join("");
+			callback(null, output);
 		}
 	});
-	
 }
 
 module.exports = {
